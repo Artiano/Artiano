8830a7b98060277bda9ae96e7e96428cec9501ee
BreezeDust <BreezeDust@qq.com>
2013-09-11 16:27:18 +0800

    +Domain区域划分对象

--- src/artiano/core/structure/Domain.java
+++ src/artiano/core/structure/Domain.java
@@ -1,8 +1,31 @@
 package artiano.core.structure;
-
+/***
+ * 
+ * @author BreezeDust
+ *
+ */
 public class Domain {
-	public Domain(){
-		
+	public double min=0;
+	public double max=0;
+	/***
+	 *  定义域划分，以min=<value<max 的形式
+	 * @param min 最小边界，区域包含min
+	 * @param max 最大边界,区域部包含max
+	 */
+	public Domain(double min,double max){
+		this.max=max;
+		this.min=min;
+	}
+	/***
+	 * 判断是否落在区域内
+	 * @param value
+	 * @return
+	 */
+	public int isIn(double value){
+		if(value>=min && value<max) return 0;
+		if(value<min) return -1;
+		if(value>=max) return 1;
+		return -2;
 	}
 
 }
