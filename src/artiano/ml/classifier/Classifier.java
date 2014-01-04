32c3ba053507c63848b4238b2046519e8e330429
NanoJeoMichael <NanoJeoMichael@hotmail.com>
2014-01-04 20:04:15 +0800

    modified core.Table

--- src/artiano/ml/classifier/Classifier.java
+++ src/artiano/ml/classifier/Classifier.java
@@ -1,22 +1,39 @@
 package artiano.ml.classifier;
 
+import artiano.core.operation.CapabilityHandler;
 import artiano.core.operation.Preservable;
 import artiano.core.structure.*;
 
-public abstract class Classifier extends Preservable {	
+public abstract class Classifier extends Preservable implements
+		CapabilityHandler {
 	private static final long serialVersionUID = 5186515619281612199L;
 
 	/**
 	 * Train data
-	 * @param trainSet data to train	 
+	 * 
+	 * @param trainSet
+	 *            data to train
 	 * @return whether the training successes or not
 	 */
 	public abstract boolean train(Table trainSet);
-	
+
 	/**
 	 * Predict label of each sample case
-	 * @param samples samples to test
-	 * @return predications of label of each sample case   
+	 * 
+	 * @param samples
+	 *            samples to test
+	 * @return predications of label of each sample case
 	 */
 	public abstract NominalAttribute predict(Table samples);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see artiano.core.operation.CapabilityHandler#capability()
+	 */
+	public Capability capability() {
+		Capability capability = new Capability();
+		capability.disableAll();
+		return capability;
+	}
 }
